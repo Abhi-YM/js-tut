2a2dbb9d075d765f1809474454c74ea2798fec61
Abhi-YM <abhi123.abhiym@gmail.com>
2026-03-10 21:37:32 +0530

    N2

---
+++ 01_basics/test.js
@@ -0,0 +1,11 @@
+console.log("yash");
+console.log("hello");
+
+//window.alert("This is an alert box!");
+
+// This is a single-line comment
+
+/*
+This is a multi-line comment
+It can span multiple lines
+*/  
